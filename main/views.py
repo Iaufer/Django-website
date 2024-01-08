9cdcc2bc36727f5f0ff5aaeb533c5c9cf2624fe4
Artyom <lauferartyom@vk.com>
2024-01-08 21:42:20 +0700

    Registered simple tag for categories

--- main/views.py
+++ main/views.py
@@ -4,13 +4,10 @@ from django.shortcuts import render
 from goods.models import Categories
 
 def index(request):
-
-    categories = Categories.objects.all()
-
+    
     context = {
         'title': 'Home - Главная',
         'content': "Магазин мебели HOME",
-        'categories': categories
     }
 
     return render(request, 'main/index.html', context)
