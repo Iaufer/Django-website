5e032d5a31ad0e6a5ca3b4c1e0d7188772ab58d2
Artyom <lauferartyom@vk.com>
2024-01-10 16:46:18 +0700

    Templates for users app added

--- app/settings.py
+++ app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     'main',
     'goods',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
