c52c8cff7a41e391f5c54e456aabcc3287661b54
Artyom <lauferartyom@vk.com>
2024-01-10 18:53:00 +0700

    Registration form and conroller are ready

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.urls import reverse
 from django.contrib import auth
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
 
@@ -28,8 +28,19 @@ def login(request):
 
 
 def registration(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
     context = {
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form
     }
 
     return render(request, 'users/registration.html', context)
@@ -44,4 +55,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
